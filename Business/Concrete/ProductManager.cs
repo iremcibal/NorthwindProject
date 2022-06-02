b12482b0c30d2bda37cd381b300a630fbfe1d78d
İrem Balcı <info@irembalci.com>
2022-06-03 01:43:33 +0300

    updated validator with aop

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Abstract;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
@@ -24,11 +25,11 @@ namespace Business.Concrete
         }
 
 
-
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
 
-            ValidationTool.Validate(new ProductValidator(), product);
+            //ValidationTool.Validate(new ProductValidator(), product);
 
             _productDal.Add(product);
 
